ccbd9839beb7750d8be111a1c5d098199d1ccec6
keroken <kenichi.shinagawa@gmail.com>
2021-07-18 23:30:20 +0900

    update fancyDiv with styled-components

--- src/components/FancyDiv.tsx
+++ src/components/FancyDiv.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const FancyDiv: React.FC = ({ children }) => {
-  return <div style={{ border: '1px solid red' }}>{children}</div>
+  return <StyledDiv>{children}</StyledDiv>
 }
+
+const StyledDiv = styled.div`
+  border: 1px solid blue;
+`;
+
 export default FancyDiv
