6af454b614b79094a45c7975dfff4887ebfd72a0
keroken <kenichi.shinagawa@gmail.com>
2021-07-20 23:08:21 +0900

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { GlobalStyle } from './GlobalStyle'
-import { Head, Root } from 'react-static'
+import { Head, Root, Routes, addPrefetchExcludes } from 'react-static'
+import { Router } from '@reach/router'
 import { Nav } from 'components//Nav'
 import { ParticlesBG } from 'components/ParticlesBG'
+import Dynamic from 'containers/Dynamic'
 import styled from 'styled-components'
+
+// Any routes that start with 'dynamic' will be treated as non-static routes
+addPrefetchExcludes(['dynamic'])
 
 function App() {
   return (
@@ -15,6 +20,12 @@ function App() {
         </Head>
         <ParticlesBG />
         <Nav />
+          <React.Suspense fallback={<em>Loading...</em>}>
+            <Router>
+              <Dynamic path="dynamic" />
+              <Routes path="*" />
+            </Router>
+          </React.Suspense>
       </StyledRoot>
     </>
   )
