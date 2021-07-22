5976390bb474f72b447d5b5108e3571fd7f4a19b
keroken <kenichi.shinagawa@gmail.com>
2021-07-22 23:23:19 +0900

    add AboutMe to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { AboutMe } from 'components/AboutMe'
 import { Header } from 'components/Header'
 import React from 'react'
 
 export default () => (
   <div>
     <Header />
+    <AboutMe />
   </div>
 )
